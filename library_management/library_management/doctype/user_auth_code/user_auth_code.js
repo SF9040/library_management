c402255e8376c88d7cce4c235752545356759b23
SF9040 <saber.chfd@gmail.com>
2024-02-03 07:34:02 +0000

    other modules

---
+++ library_management/library_management/doctype/user_auth_code/user_auth_code.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Saber and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('User Auth Code', {
+	// refresh: function(frm) {
+
+	// }
+});
