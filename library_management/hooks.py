f200d343b322ae65e34b4c0175cfcc2564c8b42a
Saber <saber.chfd@gmail.com>
2024-04-21 01:24:19 +0700

    overriding the taxes and totals.

--- library_management/hooks.py
+++ library_management/hooks.py
@@ -18,7 +18,7 @@ app_include_css = [
 # app_include_css = "/assets/library_management/css/library_management.css"
 # app_include_js = "/assets/library_management/js/library_management.js"
 app_include_js = [
-    "/assets/library_management/public/js/taxes_and_totals.js"
+    "/assets/library_management/js/taxes_and_totals.js"
 ]
 # include js, css files in header of web template
 # web_include_css = "/assets/library_management/css/library_management.css"
